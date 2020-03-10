e07f4e9365adebc6057d8a51d6f700b0e4a38de9
Ville Takala <ville.i.takala@gmail.com>
2020-03-10 21:47:16 +0200

    add test to check if game is still going on

--- src/tests/game.spec.ts
+++ src/tests/game.spec.ts
@@ -32,4 +32,14 @@ describe('should work', function () {
         expect(result.winner).toBe('x');
     });
 
+    it('should not be a hit', () => {
+        const game = createGame(6, 6, []);
+        expect(mark(game.board, 0, 2, 'x')).toBe(true);
+        expect(mark(game.board, 1, 2, 'o')).toBe(true);
+        expect(mark(game.board, 2, 2, 'x')).toBe(true);
+        const result = checkForResult(game.board, 3);
+        expect(result.finished).toBe(false);
+        expect(result.winner).toBe('');
+    });
+
 });
